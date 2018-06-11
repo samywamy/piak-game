afd79e821fdca05e9b910f790dbd45550664ff40
samywamy <sam.alexis.ng@gmail.com>
2018-06-11 15:56:45 +0800

    added html, css, gameplay idea

---
+++ script.js
@@ -0,0 +1,27 @@
+// game loads with a popup with a start button.
+// familiarize player with controls by making them press all keys on the numpad (optional)
+
+// game starts
+
+// a fly appears on a random square. Alert: Piak the fly!
+// player has to press the corresponding key the bug is on.
+// each fly gives 10 points.
+// if fly is not hit, 10 points is taken away.
+
+// when points reach 100, spider is unlocked. Alert: Piak the spider twice!
+// each spider takes 2 hits to kill but gives 20 points.
+// if spider is not hit, 20 points is taken away.
+// game gets faster
+
+// when points reach 500, sticky trap is unlocked. Alert: Don't touch the sticky trap!
+// if player hits a sticky trap, 50 points are taken away.
+
+// when points reach 1000, numbers are unlocked. Alert: Piak the NUMBER shown!
+// random number from 1-9 appears on random square.
+// player has to hit the NUMBER on the numpad and not the position.
+// gives 30 points
+// 30 points taken away if not hit.
+
+// if a wrong square is hit at any time, -20 points.
+
+// game gets faster and faster until all of player's points are lost.
